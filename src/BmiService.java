863ab6cbc8350cd3ba3c71d2c782bf4a0277b3b4
Igor Eliseev <eliseev.igor2207@gmail.com>
2024-10-08 22:42:34 +0500

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+
+    public int calculate(int weightKg, double heightMeters) {
+
+        double bmi = weightKg / (heightMeters * heightMeters);
+        return (int) bmi;
+    }
+}
